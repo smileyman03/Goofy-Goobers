9137030949cb951bdb569ab13efe8bce8a7f9a46
SmooleyMan <daniel.groeneweg03@gmail.com>
2023-05-09 09:59:46 +0200

    Made player move using vector

--- GXPEngine/Player.cs
+++ GXPEngine/Player.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using GXPEngine;
+using GXPEngine.Core;
 
 public class Player : Sprite
 {
-    private float turnSpeed = 3;
-    private float moveSpeed = 5;
+    private float turnSpeed = 5f;
+    private Vector2 velocity = new Vector2(0, -10);
     public Player() : base("triangle.png")
     {
         SetOrigin(width/2, height/2);
@@ -18,19 +21,23 @@ public class Player : Sprite
 
     void Update()
     {
+        Console.WriteLine("velocity: " + velocity.x + " " + velocity.y);
         if (Input.GetKey(Key.A))
         {
+            velocity.RotateDegrees(-5);
             rotation -= turnSpeed;
         }
         if (Input.GetKey(Key.D))
         {
+            velocity.RotateDegrees(5);
             rotation += turnSpeed;
         }
         // Check if the W and S keys are currently pressed.
         // If so, move the space ship relative to its current orientation:
         if (Input.GetKey(Key.W))
         {
-            Move(0, -moveSpeed); // move forward (=up)
+            x += velocity.x;
+            y += velocity.y;
         }
     }
 }
